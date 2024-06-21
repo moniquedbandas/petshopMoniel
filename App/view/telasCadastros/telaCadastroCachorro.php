eee738695ad6a368bdb88685fbf7bc25637a2555
Monique Dias Bandas <moniquedbandas@gmail.com>
2024-06-20 23:36:36 -0300

    Comecei a utilizar o bootstrap na pagina inicial, creio que ela será a unica que precisará de responsividade. Já estou cuidando disso com os media querys.

--- App/view/telasCadastros/telaCadastroCachorro.php
+++ App/view/telasCadastros/telaCadastroCachorro.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://kit.fontawesome.com/5bc1ff5e4a.js" crossorigin="anonymous"></script>
     <link rel="icon" href="../../../Public/imagens/patinha.png" type="image/png" sizes="16x16">
     <link rel="icon" href="../../../Public/imagens/patinha.png" type="image/png" sizes="32x32">
     <link href="https://fonts.googleapis.com/css2?family=Nunito" rel="stylesheet">
